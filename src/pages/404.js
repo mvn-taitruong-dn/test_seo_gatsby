f5a47b026a7f2f736f4eb75a1cf723ab6dc787d7
Tai Truong V <tai.truong@monstar-lab.com>
2022-06-15 15:35:59 +0700

    fix error

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,25 @@ import Layout from "../components/Layout"
 import View from "../components/View"
 import { navigate } from 'gatsby'
 
-const NotFound = () => navigate('/', {replace:true})
+
+
+const NotFound = () => 
+{
+  useEffect(()=>{
+    console.log("sdsdsa");
+    navigate('/', {replace:true})
+  },[])
+  
+  return (
+ 
+  
+
+  <Layout>
+    <View title="Not Found">
+      {/* <h3>{code}</h3> */}
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </View>
+  </Layout>
+)}
 
 export default NotFound
