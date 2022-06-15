a76b00b7fbcb5ddee2379cc983f9a8c1dbafd9d7
Tai Truong V <tai.truong@monstar-lab.com>
2022-06-15 17:27:26 +0700

    fix error

--- src/pages/404.js
+++ src/pages/404.js
@@ -13,23 +13,26 @@ const NotFound = () =>
   const location = useLocation();
   
     if (location.pathname.match(/^\/hello/)){
-
+      console.log("dsds");
       navigate('/hello', {replace:true})
+      return null;
     }else {
       navigate('/', {replace:true})
+      return null;
     };
   return null;
   
-  return (
+  // return (
  
   
 
-  <Layout>
-    <View title="Not Found">
-      {/* <h3>{code}</h3> */}
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </View>
-  </Layout>
-)}
+  // <Layout>
+  //   <View title="Not Found">
+  //     {/* <h3>{code}</h3> */}
+  //     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  //   </View>
+  // </Layout>
+// )
+}
 
 export default NotFound
