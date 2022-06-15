0531be7de5f3eb1ae7de1b2dff16b6e6e19294b1
Tai Truong V <tai.truong@monstar-lab.com>
2022-06-15 15:52:39 +0700

    fix error

--- gatsby-node.js
+++ gatsby-node.js
@@ -23,8 +23,7 @@ exports.createPages= async({actions})=>{
   })
 
  
-  createRedirect({ fromPath: "/hello/*", toPath: "/hello", isPermanent: true , redirectInBrowser: true});
-  createRedirect({ fromPath: "/*", toPath: "/", isPermanent: true , redirectInBrowser: true});
+ 
 }
 
 exports.onCreatePage = async ({ page, actions }) => {
@@ -40,6 +39,8 @@ exports.onCreatePage = async ({ page, actions }) => {
   //   // Update the page.
   //   createPage(page)
   // }
+  createRedirect({ fromPath: "/hello/*", toPath: "/hello", isPermanent: true , redirectInBrowser: true});
+  createRedirect({ fromPath: "/*", toPath: "/", isPermanent: true , redirectInBrowser: true});
   if (page.path.match(/^\/app/)) {
     console.log("page.matchPath",page.matchPath);
     console.log("page",page);
