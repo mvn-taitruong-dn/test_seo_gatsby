40c98be2050dc8d3d2e5cb248bbdcfbd7941f7ef
Tai Truong V <tai.truong@monstar-lab.com>
2022-06-20 15:35:40 +0700

    fix remove app

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,20 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Demo Simple Authentication`,
   },
-  plugins: [`gatsby-plugin-react-helmet`, 'gatsby-plugin-image', 'gatsby-plugin-offline','gatsby-plugin-client-side-redirect'],
+  plugins: [`gatsby-plugin-react-helmet`, 'gatsby-plugin-image', 'gatsby-plugin-offline','gatsby-plugin-client-side-redirect', 
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `Gatsby client-side routing only`,
+      short_name: `client-side routing`,
+      start_url: `/`, 
+      icon:'./src/images/logo.png',
+      display: `minimal-ui`
+    }
+  },
+//  {
+//       resolve: `gatsby-plugin-create-client-paths`,
+//       options: { prefixes: [`/*`] }
+//     }
+  ],
 }
--- src/pages/[...].js
+++ src/pages/[...].js
@@ -10,7 +10,7 @@ import Status from "../components/Status"
 const App = () => (
   <Layout>
     <Status />
-    <Router>
+    <Router basepath="/">
       <PrivateRoute path="/details" component={Details} />
       <PrivateRoute path="/profile" component={Profile} />
       <Login path="/login" />
