50a617fcd33bd71e0749aaca6a33f43c1c8ff2f1
Tai Truong V <tai.truong@monstar-lab.com>
2022-06-20 12:57:22 +0700

    fix remove app

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,27 +1,9 @@
 import React, { useEffect } from "react"
 import Layout from "../components/Layout"
 import View from "../components/View"
-import { navigate} from 'gatsby'
-
-
-import { useLocation } from "@reach/router"
-
-
 
 const NotFound = () => 
-{
-  // const location = useLocation();
-  
-  //   if (location.pathname.match(/^\/hello/)){
-  //     console.log("dsds");
-  //     navigate('/hello', {replace:true})
-  //     return null;
-  //   }else {
-  //     navigate('/', {replace:true})
-  //     return null;
-  //   };
-  
-  
+{ 
   return (
   <Layout>
     <View title="Not Found">
