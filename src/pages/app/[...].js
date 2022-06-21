aaa1f5cb9cbaa11ecb6e51c6164d7329cd39a9a6
Tai Truong V <tai.truong@monstar-lab.com>
2022-06-21 09:36:18 +0700

    fix app

--- src/pages/app/[...].js
+++ src/pages/app/[...].js
@@ -1,16 +1,16 @@
 import React from "react"
 import { Router, Redirect } from "@reach/router"
-import Layout from "../components/Layout"
-import Profile from "../components/Profile"
-import Details from "../components/Details"
-import Login from "../components/Login"
-import PrivateRoute from "../components/PrivateRoute"
-import Status from "../components/Status"
+import Layout from "../../components/Layout"
+import Profile from "../../components/Profile"
+import Details from "../../components/Details"
+import Login from "../../components/Login"
+import PrivateRoute from "../../components/PrivateRoute"
+import Status from "../../components/Status"
 
 const App = () => (
   <Layout>
     <Status />
-    <Router >
+    <Router basepath='/app' >
       <PrivateRoute path="/details" component={Details} />
       <PrivateRoute path="/profile" component={Profile} />
       <Login path="/login" />
