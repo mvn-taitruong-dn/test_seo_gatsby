804781e98aac5a3e6c0a920fcd1694e5dda403d6
Tai Truong V <tai.truong@monstar-lab.com>
2022-06-21 09:48:08 +0700

    fix route [...]

--- gatsby-node.js
+++ gatsby-node.js
@@ -26,68 +26,68 @@ exports.createPages= async({actions})=>{
   // createRedirect({ fromPath: "/*", toPath: "/404", isPermanent: true , redirectInBrowser: true});
 }
 
-exports.onCreatePage = async ({ page, actions }) => {
-  const { createPage, createRedirect } = actions
-    // page.matchPath is a special key that's used for matching pages
-  // only on the client.
-  // console.log("page.matchPath",page.matchPath);
-  // console.log("page",page);
-  // console.log("page.path.",page.path);
-  // if (page.path.match(/^\/)) {
+// exports.onCreatePage = async ({ page, actions }) => {
+//   const { createPage, createRedirect } = actions
+//     // page.matchPath is a special key that's used for matching pages
+//   // only on the client.
+//   // console.log("page.matchPath",page.matchPath);
+//   // console.log("page",page);
+//   // console.log("page.path.",page.path);
+//   // if (page.path.match(/^\/)) {
    
-  //   page.matchPath = `/*`
-  //   // Update the page.
-  //   createPage(page)
-  // }
+//   //   page.matchPath = `/*`
+//   //   // Update the page.
+//   //   createPage(page)
+//   // }
   
-  console.log("page.path.",page.path);
-  if (page.path.match(/^\/app/)) {
-    console.log("page.matchPath",page.matchPath);
-    console.log("page",page);
-    console.log("page.path.",page.path);
-    page.matchPath = `/app/*`
-    // Update the page.
-    await createPage(page);
-    // page.matchPath = `/profile`;
-    // // Update the page.
-    // await createPage(page)
-    // page.matchPath = `/details`;
-    // // Update the page.
-    // await createPage(page)
-  }
+//   console.log("page.path.",page.path);
+//   if (page.path.match(/^\/app/)) {
+//     console.log("page.matchPath",page.matchPath);
+//     console.log("page",page);
+//     console.log("page.path.",page.path);
+//     page.matchPath = `/app/*`
+//     // Update the page.
+//     await createPage(page);
+//     // page.matchPath = `/profile`;
+//     // // Update the page.
+//     // await createPage(page)
+//     // page.matchPath = `/details`;
+//     // // Update the page.
+//     // await createPage(page)
+//   }
 
-  // if (page.path.match(/^\/login/)) {
-  //   console.log("page.matchPath",page.matchPath);
-  //   console.log("page",page);
-  //   console.log("page.path.",page.path);
-  //   page.matchPath = `/login`
-  //   // Update the page.
-  //   createPage(page)
-  // }
-  // if (page.path.match(/^\/login/)) {
-  //   console.log("page.matchPath",page.matchPath);
-  //   console.log("page",page);
-  //   console.log("page.path.",page.path);
-  //   page.matchPath = `/login`
-  //   // Update the page.
-  //   createPage(page)
-  // }
-  // if (page.path.match(/^\/profile/)) {
-  //   console.log("page.matchPath",page.matchPath);
-  //   console.log("page",page);
-  //   console.log("page.path.",page.path);
-  //   page.matchPath = `/profile`
-  //   // Update the page.
-  //   createPage(page)
-  // }
+//   // if (page.path.match(/^\/login/)) {
+//   //   console.log("page.matchPath",page.matchPath);
+//   //   console.log("page",page);
+//   //   console.log("page.path.",page.path);
+//   //   page.matchPath = `/login`
+//   //   // Update the page.
+//   //   createPage(page)
+//   // }
+//   // if (page.path.match(/^\/login/)) {
+//   //   console.log("page.matchPath",page.matchPath);
+//   //   console.log("page",page);
+//   //   console.log("page.path.",page.path);
+//   //   page.matchPath = `/login`
+//   //   // Update the page.
+//   //   createPage(page)
+//   // }
+//   // if (page.path.match(/^\/profile/)) {
+//   //   console.log("page.matchPath",page.matchPath);
+//   //   console.log("page",page);
+//   //   console.log("page.path.",page.path);
+//   //   page.matchPath = `/profile`
+//   //   // Update the page.
+//   //   createPage(page)
+//   // }
   
-  // if (page.path.match(/^\/error\/$/)) {
-  //   console.log("sdsds");
-  //   // Match all paths starting with this code (apart from other valid paths)
-  //   page.matchPath = `/error/*`
-  //   createPage(page)
-  // }
+//   // if (page.path.match(/^\/error\/$/)) {
+//   //   console.log("sdsds");
+//   //   // Match all paths starting with this code (apart from other valid paths)
+//   //   page.matchPath = `/error/*`
+//   //   createPage(page)
+//   // }
   
 
 
-}
+// }
